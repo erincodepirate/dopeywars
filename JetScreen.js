35664265aa4e14fc1cc4d9c612cc18b54c0cca1d
Erin Clark <erincodepirate@gmail.com>
2023-02-11 12:58:33 -0800

    Add clickable cities to the jet screen

--- JetScreen.js
+++ JetScreen.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import { DataTable } from 'react-native-paper';
+
+const places = ["Bronx", "Ghetto", "Central Park", "Manhattan", "Coney Island", "Brooklyn"]
 
 class JetScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Where to?</Text>
+        <DataTable>
+          {
+            places.map(place => (
+              <DataTable.Row key={place} onPress={
+                () => {
+                  this.goToCity(place)}
+                }>
+              <DataTable.Cell>
+                  <Text>
+                    {place}
+                  </Text>
+              </DataTable.Cell>
+            </DataTable.Row>
+            ))
+          }
+        </DataTable>
 
         <Button
           title="Play"
@@ -16,7 +35,14 @@ class JetScreen extends React.Component {
       </View>
     );
   }
+
+  goToCity(place) {
+    console.log(place);
+    this.props.navigation.navigate('City');
+  }
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
