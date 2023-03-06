e428bcba1b82a574ca6249b295c76135b2197683
Erin Clark <erincodepirate@gmail.com>
2023-03-05 18:05:57 -0800

    Touch up the location selection screen

--- src/screens/JetScreen.tsx
+++ src/screens/JetScreen.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { Menu, Text } from 'react-native-paper';
+import { Card, Divider, Menu, Text, TouchableRipple } from 'react-native-paper';
 import { useDispatch, useSelector } from 'react-redux';
 import { loadCity } from '../actions/CityActions';
 import { City } from '../Enums';
@@ -9,7 +9,7 @@ import { RootState } from '../Interfaces';
 function JetScreen(props: any) {
   const dispatch = useDispatch();
   const { cityState } = useSelector((state: RootState) => state);
-  
+
   function goToCity(place: City) {
     // only load the new city if we are moving to a different city
     if (cityState.currentCity != place)
@@ -19,25 +19,31 @@ function JetScreen(props: any) {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>Where to?</Text>
+      <View style={styles.menuItem}>
+        <Divider/>
+      </View>
       {
         Object.values(City).map(place => (
-          <Menu.Item
-            key={place}
-            onPress={
-              () => {
-                goToCity(place)
-              }
-            }
-            title={place} />
+          <View style={styles.menuItem}>
+            <TouchableRipple onPress={() => { goToCity(place) }}>
+              <Text style={styles.place}>{place}</Text>
+            </TouchableRipple>
+            <Divider />
+          </View>
         ))
       }
+      <View style={styles.cityStatus}>
       {
         cityState.currentCity &&
-        <Text>
-          Current location: {cityState.currentCity}
-        </Text>
+        <Card>
+          <Card.Content>
+            <Text>
+              Current location: {cityState.currentCity}
+            </Text>
+          </Card.Content>
+        </Card>
       }
+      </View>
     </View>
   );
 }
@@ -49,14 +55,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  header: {
-    fontSize: 20
+  place: {
+    fontSize: 28,
+    textAlign: 'center'
+  },
+  menuItem: {
+    justifyContent: 'center',
+    width: "100%",
   },
   citytext: {
-
     alignContent: 'center',
     textAlign: 'center',
     justifyContent: 'center',
+  },
+  cityStatus: {
+    paddingTop: '10px'
   }
 });
 
