3f73726c3f732aba9b39167045c9b86f249a6047
Erin Clark <erincodepirate@gmail.com>
2023-06-19 20:50:16 -0700

    Add console.log optimization

--- index.js
+++ index.js
@@ -2,8 +2,12 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import Main from './Main';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+
+if (!__DEV__) {
+    console.log = () => { };
+}
 
 AppRegistry.registerComponent(appName, () => Main);
